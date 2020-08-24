404366ed90413b9be91d69168bee87db40bf1c19
Catherine Ashby <catherinashby@gmail.com>
2020-08-23 20:53:49 -0400

    added tests for models

--- core/tests.py
+++ core/tests.py
@@ -1,4 +1,26 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import SystemMessage, EventMessage, Game
+
+class ModelsTest( TestCase ):
+
+    def test_system_messages( self ):
+        txt = "system message"
+        msg = SystemMessage(message_added=timezone.now(), message_text=txt)
+        lbl = '{}'.format( msg )
+        self.assertTrue( lbl.endswith(txt) )
+
+    def test_event_messages( self ):
+        txt = "event message"
+        msg = EventMessage(event_added=timezone.now(), event_title=txt)
+        lbl = '{}'.format( msg )
+        self.assertTrue( lbl.endswith(txt) )
+
+    def test_game_object( self ):
+        txt = "game name"
+        obj = Game(game_name=txt)
+        lbl = '{}'.format(obj)
+        self.assertEqual(lbl,txt)
 
 
 class ViewsTest( TestCase ):
